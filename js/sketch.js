a306e80db9d1f8b29a45fe5e8f9d9faa21cfa4b4
Tyler Lindow <tyler.lindow@gmail.com>
2018-04-14 09:29:26 -0700

    Added header, 3 images, 3 descriptions, and a link

---
+++ js/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  fill(255);
+  stroke(255);
+  rect(0, 0, width, height);
+  fill('#3498db');
+  text('p5 sketch placeholder', 50, 50);
+}
