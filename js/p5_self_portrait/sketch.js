807af0eee212c5cad1904104a3d589f1259bae8d
Tyler Lindow <tyler.lindow@gmail.com>
2018-04-26 07:25:48 -0700

    Added intro.js first step to header. Type directly into console

--- js/p5_self_portrait/sketch.js
+++ js/p5_self_portrait/sketch.js
@@ -5,14 +5,15 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500, 500);
+  var canvas = createCanvas(120, 120);
+  canvas.parent(sketch_holder);
   turtle1 = new Turtle(150, 225, 220, 200, 18);
   turtle2 = new Turtle(100, 225, 70, 200, 20);
 }
 
 function draw() {
   background(0);
-  image(img1, 0, 0);
+  image(img1, 0, 0, 200, 282);
   turtle1.display();
   turtle1.move();
   turtle2.display();
