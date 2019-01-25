66715b77c1e9dc16efdaf65c6f5d50caa9acb8b5
Tyler Lindow <tyler.lindow@gmail.com>
2019-01-25 14:32:00 -0800

    Add "X" to menu button toggle

--- dropdown.js
+++ dropdown.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var btnMenu = document.querySelector(".btn-menu");
   btnMenu.addEventListener("click", function() {
     document.querySelector(".menu-content").classList.toggle("show");
+    document.getElementsByClassName('fas')[0].classList.toggle("hidden");
+    document.getElementsByClassName('fas')[1].classList.toggle("hidden");
   });
 });
